3d7b75a8b264c69bdd11c4157038485a993f8c4f
Sricharan Kidambi <srki3050@colorado.edu>
2022-09-11 20:01:14 -0600

    Assignment-3 Part-2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -3,6 +3,8 @@
 # Author: Sricharan Kidambi
 # Build the linux system
 # Include compiling the kernel, setting up the file system with app content.
+# Date: 11th September 2022
+# References: Mastering Embedded Linux Chapter 5, coursera Module 2 videos
 
 set -e
 set -u
@@ -22,10 +24,11 @@ else
 	OUTDIR=$1
 	echo "Using passed directory ${OUTDIR} for output"
 fi
-
+# create a directory if it doesnt exist, else no operation
 mkdir -p ${OUTDIR}
-
+# traverse to that directory
 cd "$OUTDIR"
+# 
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
     #Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
@@ -35,11 +38,25 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
-
+    wget https://github.com/torvalds/linux/commit/e33a814e772cdc36436c8c188d8c42d019fda639.diff
+    
     # TODO: Add your kernel build steps here
+    
+    #mrproper performs "deep-clean"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    #defconfig configures virtual arm dev board which will emulate QEMU
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    #Build a kernel image for booting with QEMU
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    #Build any kernel modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    #Build the devicetree
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
+# copy the image from the existing location to OUTDIR directory
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -50,7 +67,14 @@ then
 fi
 
 # TODO: Create necessary base directories
+cd "$OUTDIR"
+mkdir rootfs
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
+# Base directories created
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
@@ -58,25 +82,53 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+export SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+cp $SYSROOT/lib/ld-linux-aarch64.so.1 ${OUTPUTDIR}/rootfs/lib
+cp $SYSROOT/lib/libm.so.6 ${OUTPUTDIR}/rootfs/lib64
+cp $SYSROOT/lib/libresolv.so.2 ${OUTPUTDIR}/rootfs/lib64
+cp $SYSROOT/lib/libc.so.6 ${OUTPUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd ${FINDER_APP_DIR}
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} all
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+# references: discussed with swapnil ghonge and obtained ideas on how to copy contents to rootfs directory
+cp ${FINDER_APP_DIR}/finder.sh ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home
+cp ${FINDER_APP_DIR}/conf/ -r ${OUTDIR}/rootfs/home
 
+# References: Mastering Embedded Linux Programming chapter 5 page 199
 # TODO: Chown the root directory
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
 
+# References: Mastering Embedded Linux Programming chapter 5 page 219
 # TODO: Create initramfs.cpio.gz
+cd ${OUTDIR}/rootfs
+find .| cpio -H newc -ov --owner root:root > ../initramfs.cpio
+cd ..
+
+# Zip the OUTDIR FILE
+gzip -f initramfs.cpio
